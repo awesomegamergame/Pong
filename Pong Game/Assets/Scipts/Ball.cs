ce81ad6a67331e56c6603624458c458d967ac75d
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2020-07-14 11:34:12 -0400

    Made Ball Move

---
+++ Pong Game/Assets/Scipts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Vector2 direction;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.one.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = direction * speed;
+    }
+
+
+}
